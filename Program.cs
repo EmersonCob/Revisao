3edcb5db4c997da57ac5dc43146b436d77a6cad2
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-06-03 11:12:51 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -44,7 +44,21 @@ namespace Revisao
                         }                    
                         break;
                     case "3":
-                        //TODO: calcular media geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i = 0; i < alunos.Length; i++)
+                        {
+                           if (!string.IsNullOrEmpty(alunos[i].Nome))
+                           {
+                               notaTotal = notaTotal + alunos[i].Nota;
+                               nrAlunos++;
+                           } 
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Console.WriteLine($"Média Geral: {mediaGeral}");
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
