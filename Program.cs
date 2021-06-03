1cff04746cc9ec77cc7468cfd433b115c8e38ce9
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-06-03 10:36:05 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
             string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
@@ -13,7 +14,21 @@ namespace Revisao
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: adcionar aluno
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new AggregateException("Valor da nota deve ser decimal")
+                        }
+                        
                         break;
                     case "2":
                         //TODO: listar alunos                    
